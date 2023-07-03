4edb5b988c1d34132f53f5745996a1b98335034f
CosmicBreadCat <adhamgaweesh@gmail.com>
2023-07-03 18:37:19 +0300

    Revert "added logging in, course browsing, database and some other small fixes"
    This reverts commit d47b88acd5d28fb0db4b6276837c0fcfaa2f28d1.

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider } from '@chakra-ui/react';
+import {Routes, Route} from "react-router-dom";
+import NavBar from "./components/NavBar.js";
+import HomePage from './components/HomePage.js';
+import AboutPage from './components/AboutPage.js';
+import CoursesPage from './components/CoursesPage.js';
 
-import LoginPage from "./components/LoginPage.js";
 
 function App() {
-	return (
-		<ChakraProvider>
-			<LoginPage />
-		</ChakraProvider>
-	);
+  return (
+    <ChakraProvider>
+        <NavBar />
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/about' element={<AboutPage />} />
+          <Route path='/courses' element={<CoursesPage />} />
+        </Routes>
+    </ChakraProvider>
+  );
 }
 
 export default App;
--- client/src/components/CoursesPage.js
+++ client/src/components/CoursesPage.js
@@ -1,68 +1,23 @@
-import {
-	Box,
-	Card,
-	CardHeader,
-	CardBody,
-	Text,
-	VStack,
-	Input,
-	Center,
-} from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 
-function CoursesPage(props) {
-	const [backendData, setBackendData] = useState();
-	const [searchField, setSearchField] = useState("");
+function CoursesPage() {
+	const [backendData, setBackendData] = useState({ courses: [""] });
 
 	useEffect(() => {
-		fetch("/api/courses")
+		fetch("/api")
 			.then((response) => response.json())
 			.then((data) => {
 				setBackendData(data);
 			});
 	}, []);
 
-	const handleChange = (e) => {
-		setSearchField(e.target.value);
-	};
-
 	return (
-		<Center margin="10px">
-			<VStack marginTop="10" width="40%" spacing="5">
-				<Input
-					type="search"
-					placeholder="Search Courses"
-					onChange={handleChange}
-				/>
-				<Box style={{ overflowY: "scroll" }} width="100%" height="500">
-					{typeof backendData == "undefined" ? (
-						<Center>
-							<p>Loading....</p>
-						</Center>
-					) : (
-						backendData.data
-							.filter((course) => {
-								return course.name
-									.toLowerCase()
-									.includes(searchField.toLowerCase());
-							})
-
-							.map((course) => {
-								return (
-									<Card border="2px" borderColor="black" marginBottom="2">
-										<CardHeader borderBottom="2px" borderColor="black">
-											<Text>{course.name}</Text>
-										</CardHeader>
-										<CardBody>
-											<Text>{course.desc}</Text>
-										</CardBody>
-									</Card>
-								);
-							})
-					)}
-				</Box>
-			</VStack>
-		</Center>
+		<Box margin="10px">
+			{backendData.courses.map((c) => {
+				return <p>{c}</p>;
+			})}
+		</Box>
 	);
 }
 
